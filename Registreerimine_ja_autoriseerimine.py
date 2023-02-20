7c3c7ea62304b66a78e010e47e9a873ca5a08071
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-02-20 12:19:47 +0200

    Update Registreerimine_ja_autoriseerimine.py

--- Registreerimine_ja_autoriseerimine.py
+++ Registreerimine_ja_autoriseerimine.py
@@ -5,32 +5,32 @@ users=["user1", "user2"]
 passw=["12345", "qwerty"]
 
 while True:
-    print("Если вы хотите пройти регистрацию нажмите - 1")
-    print("Если вы хотите авторизироваться нажмите - 2")
-    print("Если вы хотите изменить имя пользователя нажмите - 3")
-    print("Если вы хотите изменить пароль нажмите - 4")
-    print("Если вы хотите сбросить пароль нажмите - 5")
-    print("Если вы хотите закончить работу нажмите - 6")
-    выбор = int(input("Ваш выбор? "))
+    print("Kui soovite registreeruda, vajutage - 1")
+    print("Kui soovite sisse logida, vajutage - 2")
+    print("Kui soovite muuta kasutajanime, vajutage - 3")
+    print("Kui soovite salasõna muuta, vajutage - 4")
+    print("Kui soovite salasõna lähtestada, vajutage - 5")
+    print("Kui soovite töö lõpetada, vajutage - 6")
+    выбор = int(input("Teie valik? "))
 
     if выбор == 1:
         Регистрация(users, passw)
     elif выбор == 2:
         Авторизация(users, passw)
     elif выбор == 3:
-        старыйлогин = input("Введите текущее имя пользователя: ")
-        новыйлогин = input("Введите новое имя пользователя: ")
+        старыйлогин = input("Sisestage praegune kasutajanimi: ")
+        новыйлогин = input("Sisestage uus kasutajanimi: ")
         изменение_имени_пользователя(users, старыйлогин, новыйлогин)
     elif выбор == 4:
-        логин = input("Введите имя пользователя: ")
-        старыйпароль = input("Введите старый пароль: ")
-        новыйпароль = input("Введите новый пароль: ")
+        логин = input("Sisestage oma kasutajanimi: ")
+        старыйпароль = input("Sisestage vana parool: ")
+        новыйпароль = input("Sisestage uus parool: ")
         изменение_пароля(users, passw, логин, старыйпароль, новыйпароль)
     elif выбор ==5:
-        логин= input("Введите имя пользователя: ")
+        логин= input("Sisestage oma kasutajanimi: ")
         сброс_пароля(passw, логин, users)
     elif выбор == 6:
-        print("Вы выбрали закончить работу.")
+        print("Te olete otsustanud töö lõpetada.")
         break
 
 
