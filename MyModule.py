123993f568787a4307f2678b2d4aa09a608e34b3
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-02-20 11:07:12 +0200

    Update MyModule.py

--- MyModule.py
+++ MyModule.py
@@ -55,11 +55,11 @@ def Авторизация(users, passw):
         
         
         
-def изменение_имени_пользователя(users, old_name, new_name):
+def изменение_имени_пользователя(users, старыйлогин, новыйлогин):
     """Изменяет имя пользователя"""
-    if old_name in users:
-        index = users.index(old_name)
-        users[index] = new_name
+    if старыйлогин in users:
+        index = users.index(старыйлогин)
+        users[index] = новыйлогин
         print("Имя пользователя успешно изменено.")
     else:
         print("Пользователь с таким именем не найден.")
@@ -69,11 +69,11 @@ def изменение_имени_пользователя(users, old_name, new_
         
         
         
-def изменение_пароля(passw, user, old_pass, new_pass):
+def изменение_пароля(users, passw, логин, старыйпароль, новыйпароль):
     """Изменяет пароль пользователя"""
-    if user in users and old_pass in passw:
-        index = users.index(user)
-        passw[index] = new_pass
+    if логин in users and старыйпароль in passw:
+        index = users.index(логин)
+        passw[index] = новыйпароль
         print("Пароль пользователя успешно изменен.")
     else:
         print("Неверное имя пользователя или пароль.")
@@ -81,12 +81,12 @@ def изменение_пароля(passw, user, old_pass, new_pass):
         
         
         
-def сброс_пароля(passw, user):
+def сброс_пароля(passw, логин, users):
     """Сбрасывает пароль пользователя"""
-    if user in users:
-        index = users.index(user)
+    if логин in users:
+        index = users.index(логин)
         new_pass = авто_пароль()
         passw[index] = new_pass
-        print(f"Новый пароль для пользователя {user}: {new_pass}")
+        print(f"Новый пароль для пользователя {логин}: {new_pass}")
     else:
         print("Пользователь с таким именем не найден.")
